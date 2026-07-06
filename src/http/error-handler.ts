d6d3d08ace6d594fe2b6c33bcce60eef23a5c98f
Chinmay Panda <chinmay@openautonomyx.com>
2026-07-06 08:09:34 -0700

    Add HTTP error handler

---
+++ src/http/error-handler.ts
@@ -0,0 +1,17 @@
+import type { FastifyError, FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { InvalidTransitionError, NotFoundError } from "../domain/errors.js";
+
+export function registerErrorHandler(app: FastifyInstance): void {
+  app.setErrorHandler((error: FastifyError, _request: FastifyRequest, reply: FastifyReply) => {
+    if (error instanceof NotFoundError) {
+      return reply.code(404).send({ error: "not_found", message: error.message });
+    }
+
+    if (error instanceof InvalidTransitionError) {
+      return reply.code(409).send({ error: "invalid_transition", message: error.message });
+    }
+
+    app.log.error(error);
+    return reply.code(500).send({ error: "internal_error", message: "Internal server error" });
+  });
+}
